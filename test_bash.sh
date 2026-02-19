51efc6248355b5068f6ae8c667837297a5bf72a8
Andrea Bettarini <bettarini@monema.it>
2026-02-19 11:11:31 +0100

    Prova fix

--- test_bash.sh
+++ test_bash.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
 # I parametri sono passati come var1=val1 var2=val2 ... al comando di esecuzione dello script (es: via API o survey vars).
 
 for arg in "$@"; do
